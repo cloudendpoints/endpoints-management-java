59dea221d06ff4f56bf0a3624702141ba68a7160
Yang Guan <yangguan@google.com>
2016-08-04 11:39:01 -0700

    Memoize the service config fetched from Service Management
    This should avoid duplicate calls to the API (i.e. from ConfigFilter
    and from the auth Authenticator).
    
    Change-Id: I3e098d3065f7aab7102d617dd1891bc95acf5019

--- endpoints-service-config/src/test/java/com/google/api/config/ServiceConfigFetcherTest.java
+++ endpoints-service-config/src/test/java/com/google/api/config/ServiceConfigFetcherTest.java
@@ -13,184 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.api.config;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.anyListOf;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.api.Service;
-import com.google.api.client.http.LowLevelHttpRequest;
-import com.google.api.client.http.LowLevelHttpResponse;
-import com.google.api.client.json.Json;
-import com.google.api.client.testing.http.MockHttpTransport;
-import com.google.api.client.testing.http.MockLowLevelHttpRequest;
-import com.google.api.client.testing.http.MockLowLevelHttpResponse;
-import com.google.appengine.api.appidentity.AppIdentityService;
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.util.JsonFormat;
+import com.google.common.base.Supplier;
 
-import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-
 /**
- * Tests for {@link ServiceConfigFetcher}.
+ * Test for {@link ServiceConfigFetcher}.
  */
-public final class ServiceConfigFetcherTest {
-
-  private static final String ACCESS_TOKEN = "test-access-token";
-  private static final String SERVICE_NAME = "test-service-name";
-  private static final String SERVICE_VERSION = "test-service-version";
-
-  private static final Service SERVICE = Service.newBuilder()
-      .setName(SERVICE_NAME)
-      .setId(SERVICE_VERSION)
-      .build();
-
-  private final AppIdentityService mockAppIdentityService =
-      mock(AppIdentityService.class, RETURNS_DEEP_STUBS);
-  private final Environment mockEnvironment = mock(Environment.class);
-  private final TestingHttpTransport testHttpTransport = new TestingHttpTransport();
-  private final ServiceConfigFetcher fetcher = new ServiceConfigFetcher(
-      mockAppIdentityService,
-      mockEnvironment,
-      testHttpTransport);
+public class ServiceConfigFetcherTest {
+  private final Service service = Service.newBuilder().build();
+  @SuppressWarnings("unchecked")
+  private final Supplier<Service> supplier = (Supplier<Service>) mock(Supplier.class);
+  private final ServiceConfigFetcher fetcher = new ServiceConfigFetcher(supplier);
 
   @Before
   public void setUp() {
-    when(mockAppIdentityService.getAccessToken(anyListOf(String.class)).getAccessToken())
-        .thenReturn(ACCESS_TOKEN);
-    testHttpTransport.reset();
   }
 
   @Test
-  public void testServiceNameNotSet() throws IOException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn("");
-    try {
-      fetcher.fetch();
-      fail();
-    } catch (IllegalArgumentException exception) {
-      assertEquals(
-          "Environment variable 'ENDPOINTS_SERVICE_NAME' is not set",
-          exception.getMessage());
-    }
-  }
+  public void testFetchWithMemorization() {
+    when(supplier.get()).thenReturn(service);
 
-  @Test
-  public void testServiceVersionNotSet() throws IOException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn(SERVICE_NAME);
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_VERSION")).thenReturn(null);
-    try {
-      fetcher.fetch();
-      fail();
-    } catch (IllegalArgumentException exception) {
-      assertEquals(
-          "Environment variable 'ENDPOINTS_SERVICE_VERSION' is not set",
-          exception.getMessage());
-    }
-  }
+    assertEquals(service, this.fetcher.fetch());
+    assertEquals(service, this.fetcher.fetch());
 
-  @Test
-  public void testFetchSuccessfully() throws InvalidProtocolBufferException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn(SERVICE_NAME);
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_VERSION")).thenReturn(SERVICE_VERSION);
-
-    String content = JsonFormat.printer().print(SERVICE);
-    testHttpTransport.setStatusCode(200);
-    testHttpTransport.setContent(content);
-
-    assertEquals(SERVICE, fetcher.fetch());
-  }
-
-  @Test
-  public void testFetchConfigWithWrongServiceName() throws InvalidProtocolBufferException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn(SERVICE_NAME);
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_VERSION")).thenReturn(SERVICE_VERSION);
-
-    Service service = Service.newBuilder().setName("random-name").build();
-    String content = JsonFormat.printer().print(service);
-    testHttpTransport.setStatusCode(200);
-    testHttpTransport.setContent(content);
-
-    try {
-      fetcher.fetch();
-      fail();
-    } catch (ServiceConfigException exception) {
-      assertEquals("Unexpected service name in service config: random-name", exception.getMessage());
-    }
-  }
-
-  @Test
-  public void testFetchConfigWithWrongServiceVersion() throws InvalidProtocolBufferException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn(SERVICE_NAME);
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_VERSION")).thenReturn(SERVICE_VERSION);
-
-    Service service = Service.newBuilder()
-        .setName(SERVICE_NAME)
-        .setId("random-version")
-        .build();
-    String content = JsonFormat.printer().print(service);
-    testHttpTransport.setStatusCode(200);
-    testHttpTransport.setContent(content);
-
-    try {
-      fetcher.fetch();
-      fail();
-    } catch (ServiceConfigException exception) {
-      assertEquals("Unexpected service version in service config: random-version", exception.getMessage());
-    }
-  }
-
-  @Test
-  public void testFetchFailed() throws IOException {
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_NAME")).thenReturn(SERVICE_NAME);
-    when(mockEnvironment.getVariable("ENDPOINTS_SERVICE_VERSION")).thenReturn(SERVICE_VERSION);
-    testHttpTransport.setStatusCode(404);
-
-    try {
-      fetcher.fetch();
-      fail();
-    } catch (ServiceConfigException exception) {
-      assertEquals("Failed to fetch service config (status code 404)", exception.getMessage());
-    }
-  }
-
-  private static final class TestingHttpTransport extends MockHttpTransport {
-    private String content;
-    private int statusCode;
-
-    public void setContent(String content) {
-      this.content = content;
-    }
-
-    public void setStatusCode(int statusCode) {
-      this.statusCode = statusCode;
-    }
-
-    public void reset() {
-      this.content = null;
-      this.statusCode = HttpStatus.SC_OK;
-    }
-
-    @Override
-    public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
-      return new MockLowLevelHttpRequest() {
-        @Override
-        public LowLevelHttpResponse execute() throws IOException {
-          MockLowLevelHttpResponse response = new MockLowLevelHttpResponse();
-          response.setStatusCode(statusCode);
-          response.setContentType(Json.MEDIA_TYPE);
-          response.setContent(content);
-          return response;
-        }
-      };
-    }
+    verify(supplier, only()).get();
   }
 }
