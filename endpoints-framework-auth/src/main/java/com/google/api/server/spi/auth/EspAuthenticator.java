6e9e302d3ff3d007454c6d567f5f0b64af91d383
Daniel Tang <tangd@google.com>
2016-08-17 10:10:57 -0700

    add endpoints-framework-auth artifact
    Rather than shipping EspAuthenticator with the core framework
    artifact, we create a new "bridge" artifact which contains it. The
    core library can't take dependencies on the management artifacts,
    and we had to do some nasty workaround to add a dependency to the
    tools artifact. Moving EspAuthenticator outside of the core JAR
    also takes it off of the system classpath for tools, which means
    it should run without any workaround. Frameworks users will depend
    on this JAR rather than endpoints-management-auth.
    
    This is added to the management repository to share version numbers
    with the management artifacts for consistency.
    
    Change-Id: I0c6fa4264054743fef1c481b0ef8f3cf64ef7881

---
+++ endpoints-framework-auth/src/main/java/com/google/api/server/spi/auth/EspAuthenticator.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.server.spi.auth;
+
+import com.google.api.Service;
+import com.google.api.auth.Authenticator;
+import com.google.api.auth.UnauthenticatedException;
+import com.google.api.auth.UserInfo;
+import com.google.api.control.ConfigFilter;
+import com.google.api.control.model.MethodRegistry.AuthInfo;
+import com.google.api.control.model.MethodRegistry.Info;
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.config.Singleton;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Authenticator that extracts auth token from the HTTP authorization header or
+ * from the "access_token" query parameter.
+ *
+ * This authenticator supports the same authentication feature as in Endpoints
+ * Server Proxy.
+ *
+ * This authenticator needs to be placed behind {@link ConfigFilter} which adds
+ * {@link Info} and {@link Service} as attributes of the incoming HTTP requests.
+ */
+@Singleton
+public final class EspAuthenticator implements com.google.api.server.spi.config.Authenticator {
+
+  private static final Logger logger = Logger.getLogger(EspAuthenticator.class.getName());
+
+  private final Authenticator authenticator;
+
+  public EspAuthenticator() {
+    this(Authenticator.create());
+  }
+
+  @VisibleForTesting
+  EspAuthenticator(Authenticator authenticator) {
+    this.authenticator = authenticator;
+  }
+
+
+  @Override
+  public User authenticate(HttpServletRequest request) {
+    Info methodInfo = ConfigFilter.getMethodInfo(request);
+    if (methodInfo == null) {
+      throw new IllegalStateException("method_info is not set in the request");
+    }
+    Optional<AuthInfo> authInfo = methodInfo.getAuthInfo();
+    if (!authInfo.isPresent()) {
+      logger.info("auth is not configured for this request");
+      return null;
+    }
+
+    Service service = ConfigFilter.getService(request);
+    if (service == null) {
+      throw new IllegalStateException("service is not set in the request");
+    }
+
+    String serviceName = service.getName();
+
+    try {
+      UserInfo userInfo = this.authenticator.authenticate(request, authInfo.get(), serviceName);
+      return new User(userInfo.getId(), userInfo.getEmail());
+    } catch (UnauthenticatedException | UncheckedExecutionException exception) {
+      logger.warning(String.format("Authentication failed: %s", exception));
+      return null;
+    }
+  }
+}
---
+++ endpoints-framework-auth/src/test/java/com/google/api/server/spi/auth/EspAuthenticatorTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.server.spi.auth;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.api.Service;
+import com.google.api.auth.Authenticator;
+import com.google.api.auth.UnauthenticatedException;
+import com.google.api.auth.UserInfo;
+import com.google.api.control.ConfigFilter;
+import com.google.api.control.model.MethodRegistry.AuthInfo;
+import com.google.api.control.model.MethodRegistry.Info;
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.request.Attribute;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Test for {@link EspAuthenticator}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class EspAuthenticatorTest {
+  private static final String ATTRIBUTE_PREFIX = ConfigFilter.class.getName();
+  private static final String SERVICE_NAME = "service_name";
+  private static final Service SERVICE = Service.newBuilder().setName(SERVICE_NAME).build();
+
+  private static final AuthInfo AUTH_INFO = new AuthInfo(ImmutableMap.<String, Set<String>>of());
+  private static final Info INFO = new Info("selector", AUTH_INFO);
+
+  @Mock private Authenticator authenticator;
+
+  private Attribute attribute;
+  private EspAuthenticator espAuthenticator;
+  private HttpServletRequest request;
+
+  @Before
+  public void setUp() {
+    this.espAuthenticator = new EspAuthenticator(authenticator);
+    this.request = new MockHttpServletRequest();
+    this.attribute = Attribute.from(request);
+  }
+
+  @Test
+  public void testNoMethodInfo() {
+    try {
+      this.espAuthenticator.authenticate(request);
+      fail("Expected IllegalStateException.");
+    } catch (IllegalStateException exception) {
+      assertEquals("method_info is not set in the request", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testNoService() {
+    this.attribute.set(ATTRIBUTE_PREFIX + ".method_info", INFO);
+    try {
+      espAuthenticator.authenticate(request);
+      fail("Expected IllegalStateException.");
+    } catch (IllegalStateException exception) {
+      assertEquals("service is not set in the request", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testAuthenticateSuccess() {
+    String email = "user@email.com";
+    String id = "user-id";
+    UserInfo userInfo = new UserInfo(ImmutableList.<String>of(), email, id, "issuer");
+    when(this.authenticator.authenticate(request, AUTH_INFO, SERVICE_NAME))
+        .thenReturn(userInfo);
+    this.attribute.set(ATTRIBUTE_PREFIX + ".method_info", INFO);
+    this.attribute.set(ATTRIBUTE_PREFIX + ".service", SERVICE);
+
+    User user = this.espAuthenticator.authenticate(request);
+    assertEquals(email, user.getEmail());
+    assertEquals(id, user.getId());
+
+    verify(this.authenticator, only()).authenticate(request, AUTH_INFO, SERVICE_NAME);
+  }
+
+  @Test
+  public void testNoAuthInfo() {
+    Info info = new Info("selector", null);
+    this.attribute.set(ATTRIBUTE_PREFIX + ".method_info", info);
+    assertNull(this.espAuthenticator.authenticate(request));
+    verify(this.authenticator, never())
+        .authenticate(any(HttpServletRequest.class), any(AuthInfo.class), anyString());
+  }
+
+  @Test
+  public void testHandleUnauthenticatedException() {
+    when(this.authenticator.authenticate(eq(request), any(AuthInfo.class), anyString()))
+        .thenThrow(new UnauthenticatedException());
+    this.attribute.set(ATTRIBUTE_PREFIX + ".method_info", INFO);
+    this.attribute.set(ATTRIBUTE_PREFIX + ".service", SERVICE);
+    assertNull(this.espAuthenticator.authenticate(request));
+  }
+
+  @Test
+  public void testHandleUncheckedExecutionException() {
+    when(this.authenticator.authenticate(eq(request), any(AuthInfo.class), anyString()))
+        .thenThrow(new UncheckedExecutionException(new UnauthenticatedException()));
+    this.attribute.set(ATTRIBUTE_PREFIX + ".method_info", INFO);
+    this.attribute.set(ATTRIBUTE_PREFIX + ".service", SERVICE);
+    assertNull(this.espAuthenticator.authenticate(request));
+  }
+}
